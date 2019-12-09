cef986e9f8fe708d81d3ab0c21569c5f4bb5b8df
erksters <erksters@F1092-06-2094>
2019-12-09 14:21:16 -0600

    Added operator overloading

--- VectorMath/Program.cs
+++ VectorMath/Program.cs
@@ -7,6 +7,20 @@ namespace VectorMath
         static void Main(string[] args)
         {
             Console.WriteLine("Fun with vectors");
+
+            Vector3 one = new Vector3(1, 2, 3);
+            Vector3 two = new Vector3(2, 3, 4);
+
+            Console.WriteLine($"Adding two vectos {Vector3.Add(one , two)}");
+            Console.WriteLine($"With Operator overloading {Vector3.Add(one , two)}");
+
+            //one.Add(two);
+
+            Console.WriteLine($"With a class method {one}");
+            Console.WriteLine($"Veector Scaling {one * 10} ");
+            Console.WriteLine($"Vector Comparison {one == two}");
+            Console.WriteLine($"Vector Comparison again {one == new Vector3(5, 4 ,3 )}");
+
         }
     }
 }
--- VectorMath/Vector3.cs
+++ VectorMath/Vector3.cs
@@ -18,11 +18,40 @@ namespace VectorMath
         }
 
         // Vector Addition
+        public static Vector3 Add(Vector3 a, Vector3 b)
+        {
+            return new Vector3(a.X + b.X, a.Y + b.Y, a.Z + b.Z);
+        }
+
+        public static Vector3 operator +(Vector3 a, Vector3 b)
+        {
+            return new Vector3(a.X + b.X, a.Y + b.Y, a.Z + b.Z);
+        }
 
         // Vector Subtraction 
-
+        public static Vector3 operator -(Vector3 a, Vector3 b)
+        {
+            return new Vector3(a.X - b.X, a.Y - b.Y, a.Z - b.Z);
+        }
         // Vector Scaling 
+        public static Vector3 operator *(Double a, Vector3 b)
+        {
+            return new Vector3(a * b.X, a * b.Y, a * b.Z);
+        }
+        public static Vector3 operator *(Vector3 b, Double a )
+        {
+            return new Vector3(a * b.X, a * b.Y, a * b.Z);
+        }
 
         // Vector Comparison
+        public static bool operator ==(Vector3 a, Vector3 b)
+        {
+            return (a.X == b.X && a.Y == b.Y && a.Z == b.Z);
+        }
+
+        public static bool operator !=(Vector3 a, Vector3 b)
+        {
+            return (a.X != b.X || a.Y != b.Y || a.Z != b.Z);
+        }
     }
 }
